7e26df8100300535fd0ba989413dd7721f03b089
elwaheb <fadymoheb945@gmail.com>
2026-09-25 18:46:29 +0300

    Fix typescript build errors

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -6,18 +6,20 @@ import { logger } from "./lib/logger";
 
 const app: Express = express();
 
+const pinoHttpFn: any = (pinoHttp as any)?.default || pinoHttp;
+
 app.use(
-  pinoHttp({
+  pinoHttpFn({
     logger,
     serializers: {
-      req(req) {
+      req(req: any) {
         return {
           id: req.id,
           method: req.method,
           url: req.url?.split("?")[0],
         };
       },
-      res(res) {
+      res(res: any) {
         return {
           statusCode: res.statusCode,
         };
